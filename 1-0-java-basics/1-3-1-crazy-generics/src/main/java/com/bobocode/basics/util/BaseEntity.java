624658279ba87b6ecd7ec6a796a930479224fe66
Ostapiak Mykhailo <ostapiakmz@gmail.com>
2026-07-02 20:16:33 +0300

    Add: module 1-3-0 and 1-3-1

--- 1-0-java-basics/1-3-1-crazy-generics/src/main/java/com/bobocode/basics/util/BaseEntity.java
+++ 1-0-java-basics/1-3-1-crazy-generics/src/main/java/com/bobocode/basics/util/BaseEntity.java
@@ -3,7 +3,6 @@ package com.bobocode.basics.util;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
